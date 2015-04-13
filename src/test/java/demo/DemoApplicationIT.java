5588a7a6b773c087b8364954dd93c6c77c2b5a3b
catalin <lazcatluc@gmail.com>
2015-04-13 11:13:34 +0300

    add infinitest filters

--- src/test/java/demo/DemoApplicationIT.java
+++ src/test/java/demo/DemoApplicationIT.java
@@ -9,11 +9,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 @WebAppConfiguration
-public class DemoApplicationTests {
+public class DemoApplicationIT {
 
 	@Test
 	public void contextLoads() {
-		
 	}
 
 }
